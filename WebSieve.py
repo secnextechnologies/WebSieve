b81c9ffd8cc0636087c79965edd81c450b659348
SecneX Technologies <102068563+secnextechnologies@users.noreply.github.com>
2023-05-09 10:52:02 +0530

    Update WebSieve.py
    Version 1.2

--- WebSieve.py
+++ WebSieve.py
@@ -13,7 +13,7 @@ print("""
        \/       \/    \/        \/         \/          \/ 
        				- SecneX Technologies (secnex.tech)
        				
-[INF] Current WebSieve version v1
+[INF] Current WebSieve version v1.2
 """)
 
 # Set up command line arguments
@@ -30,36 +30,42 @@ if not args.url:
     parser.print_help()
     exit()
 
-# Convert cookies string to a dictionary
-cookies_dict = {}
-if args.cookies:
-    for cookie in args.cookies.split("; "):
-        key, value = cookie.split("=")
-        cookies_dict[key] = value
+try:
+    # Convert cookies string to a dictionary
+    cookies_dict = {}
+    if args.cookies:
+        for cookie in args.cookies.split("; "):
+            key, value = cookie.split("=")
+            cookies_dict[key] = value
 
-# Convert cookies dictionary to a RequestsCookieJar object
-cookies = cookiejar_from_dict(cookies_dict)
+    # Convert cookies dictionary to a RequestsCookieJar object
+    cookies = cookiejar_from_dict(cookies_dict)
 
-# Send request and extract URLs
-headers = {
-    "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.110 Safari/537.3"
-}
-response = requests.get(args.url, headers=headers, cookies=cookies)
-soup = BeautifulSoup(response.content, "html.parser")
-urls = set()  # use a set to store unique URLs
+    # Send request and extract URLs
+    headers = {
+        "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.110 Safari/537.3"
+    }
+    response = requests.get(args.url, headers=headers, cookies=cookies)
+    response.raise_for_status()  # Raise an exception for HTTP errors (e.g., 404)
+    
+    soup = BeautifulSoup(response.content, "html.parser")
+    urls = set()  # use a set to store unique URLs
 
-for link in soup.find_all('a'):
-    href = link.get('href')
-    if href and href.startswith("http"):
-        urls.add(href)
+    for link in soup.find_all('a'):
+        href = link.get('href')
+        if href and href.startswith("http"):
+            urls.add(href)
 
-url_list = "\n".join([url for url in urls])
-print(url_list)
-
-# Output or save the result
-if args.output:
-    with open(args.output, "w") as f:
-        f.write(url_list)
-        print(f"\nURLs saved to {args.output}")
-else:
+    url_list = "\n".join([url for url in urls])
     print(url_list)
+
+    # Output or save the result
+    if args.output:
+        with open(args.output, "w") as f:
+            f.write(url_list)
+            print(f"\nURLs saved to {args.output}")
+    else:
+        print(url_list)
+
+except requests.exceptions.RequestException as e:
+    print(f"Error: {str(e)}")
